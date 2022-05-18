238003e46a57f5d1ee516afb7e2492d9182769a7
vincent-k-sm <kimtmdals@gmail.com>
2022-05-18 11:14:32 +0900

    Update Presenter Template

--- Clean Swift/Clean Swift/Presenter.xctemplate/___FILEBASENAME___Presenter.swift
+++ Clean Swift/Clean Swift/Presenter.xctemplate/___FILEBASENAME___Presenter.swift
@@ -9,14 +9,19 @@ protocol ___VARIABLE_sceneName___PresentationLogic {
     func presentSomething(response: ___VARIABLE_sceneName___.Something.Response)
 }
 
-class ___VARIABLE_sceneName___Presenter: ___VARIABLE_sceneName___PresentationLogic {
+class ___VARIABLE_sceneName___Presenter {
     weak var viewController: ___VARIABLE_sceneName___DisplayLogic?
+
+    // MARK: Parse and calc respnse from ___VARIABLE_sceneName___Interactor and send simple view model to ___VARIABLE_sceneName___ViewController to be displayed
 
     deinit {
         //
     }
-    // MARK: Parse and calc respnse from ___VARIABLE_sceneName___Interactor and send simple view model to ___VARIABLE_sceneName___ViewController to be displayed
+    
+}
 
+extension ___VARIABLE_sceneName___Presenter: ___VARIABLE_sceneName___PresentationLogic {
+    
     func presentSomething(response: ___VARIABLE_sceneName___.Something.Response) {
         let viewModel = ___VARIABLE_sceneName___.Something.ViewModel()
         viewController?.displaySomething(viewModel: viewModel)
--- Clean Swift/Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Presenter.swift
+++ Clean Swift/Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Presenter.swift
@@ -9,7 +9,7 @@ protocol ___VARIABLE_sceneName___PresentationLogic {
     func presentSomething(response: ___VARIABLE_sceneName___.Something.Response)
 }
 
-class ___VARIABLE_sceneName___Presenter: ___VARIABLE_sceneName___PresentationLogic {
+class ___VARIABLE_sceneName___Presenter {
     weak var viewController: ___VARIABLE_sceneName___DisplayLogic?
 
     // MARK: Parse and calc respnse from ___VARIABLE_sceneName___Interactor and send simple view model to ___VARIABLE_sceneName___ViewController to be displayed
@@ -17,6 +17,10 @@ class ___VARIABLE_sceneName___Presenter: ___VARIABLE_sceneName___PresentationLog
     deinit {
         //
     }
+    
+}
+
+extension ___VARIABLE_sceneName___Presenter: ___VARIABLE_sceneName___PresentationLogic {
     
     func presentSomething(response: ___VARIABLE_sceneName___.Something.Response) {
         let viewModel = ___VARIABLE_sceneName___.Something.ViewModel()
