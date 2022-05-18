ccdacd3fb51148807ed831cd5378356eb01322e4
vincent-k-sm <kimtmdals@gmail.com>
2022-05-18 14:46:04 +0900

    Update Router templates

--- Clean Swift/Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Router.swift
+++ Clean Swift/Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Router.swift
@@ -13,7 +13,7 @@ protocol ___VARIABLE_sceneName___DataPassing {
     var dataStore: ___VARIABLE_sceneName___DataStore? { get }
 }
 
-class ___VARIABLE_sceneName___Router: NSObject, ___VARIABLE_sceneName___RoutingLogic, ___VARIABLE_sceneName___DataPassing {
+class ___VARIABLE_sceneName___Router: TransitionRouter, ___VARIABLE_sceneName___RoutingLogic, ___VARIABLE_sceneName___DataPassing {
     weak var viewController: ___VARIABLE_sceneName___ViewController?
     var dataStore: ___VARIABLE_sceneName___DataStore?
 
